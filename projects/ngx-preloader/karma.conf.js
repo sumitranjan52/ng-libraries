346f71c1f9bd3268fe48b17e002b8174010e2a0c
Sumit Ranjan <sumitranjan52@gmail.com>
2021-12-18 22:39:37 +0530

    converting to public library 'ngx-preloader'

--- projects/ngx-preloader/karma.conf.js
+++ projects/ngx-preloader/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../../coverage/sumitranjan52/preloader'),
+      dir: require('path').join(__dirname, '../../coverage/ngx-preloader'),
       subdir: '.',
       reporters: [
         { type: 'html' },

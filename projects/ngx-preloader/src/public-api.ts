e079b9f399e42bfef6f9229841cdeca510cdf919
Sumit Ranjan <sumitranjan52@gmail.com>
2021-12-19 20:49:24 +0530

    added to public api

--- projects/ngx-preloader/src/public-api.ts
+++ projects/ngx-preloader/src/public-api.ts
@@ -9,3 +9,7 @@ export * from './lib/flat-loader/flat-loader.module';
 export * from './lib/flat-loader/flat-loader.component';
 export * from './lib/zooming/zooming.module';
 export * from './lib/zooming/zooming.component';
+export * from './lib/wave/wave.module';
+export * from './lib/wave/wave.component';
+export * from './lib/bouncy-circle/bouncy-circle.module';
+export * from './lib/bouncy-circle/bouncy-circle.component';
